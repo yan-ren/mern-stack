bfa81a275197a3b096acac687cbd5a96900be97e
Yan Ren <ryan.ren3@gmail.com>
2021-03-29 23:32:22 -0400

    finish react-template

--- react-template/src/components/Header.js
+++ react-template/src/components/Header.js
@@ -1,10 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Button from "./Button";
+import { useLocation } from "react-router-dom";
 
-const Header = ({ title }) => {
+const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation();
+
   return (
-    <header>
+    <header className="header">
       <h1>{title}</h1>
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "Red" : "Green"}
+          text={showAdd ? "Close" : "Add"}
+          onClick={onAdd}
+        ></Button>
+      )}
     </header>
   );
 };
